1a19b0e192415f94e0aef4a4cebf243414fce7eb
navi3002 <navithapzal.mohamedrafee@chainsys.com>
2022-04-01 09:30:14 +0530

    First Commit

--- src/js lessons/rt-pcr-application-form/reset.js
+++ src/js lessons/rt-pcr-application-form/reset.js
@@ -1,5 +1,5 @@
-function reset() {
-  var table, tr;
+export function reset() {
+  var table, tr, i;
   table = document.getElementById('table1');
   tr = table.getElementsByTagName('tr');
   for (i = 1; i < tr.length; i++) {
